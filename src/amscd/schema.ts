bc33665528804b3923d123792d6c9804d5143044
Paolo Brasolin <paolo.brasolin@gmail.com>
2021-12-15 16:32:46 +0100

    feat: amscd schema

--- src/amscd/schema.ts
+++ src/amscd/schema.ts
@@ -1,3 +1,13 @@
+import {
+  array,
+  enums,
+  literal,
+  optional,
+  string,
+  tuple,
+  union,
+} from "superstruct";
+
 export enum EdgeKind {
   Empty,
   HorizontalEquals,
@@ -7,3 +17,33 @@ export enum EdgeKind {
   LeftArrow,
   RightArrow,
 }
+
+export const Vertex = string();
+
+export const HorizontalEdge = tuple([
+  enums([
+    EdgeKind.Empty,
+    EdgeKind.HorizontalEquals,
+    EdgeKind.LeftArrow,
+    EdgeKind.RightArrow,
+  ]),
+  optional(string()),
+  optional(string()),
+]);
+
+export const VerticalEdge = tuple([
+  enums([
+    EdgeKind.Empty,
+    EdgeKind.VerticalEquals,
+    EdgeKind.UpArrow,
+    EdgeKind.DownArrow,
+  ]),
+  optional(string()),
+  optional(string()),
+]);
+
+export const Blank = literal(null);
+
+export const Cell = union([Vertex, HorizontalEdge, VerticalEdge, Blank]);
+
+export const Matrix = array(array(Cell));
